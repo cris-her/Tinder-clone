77f0c01ca12a70eed97d20f380922c8a82da0651
chernandez <chernandez@hasar.com.mx>
2022-02-08 15:09:56 -0600

    AuthProvider

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,11 +1,20 @@
 import { View, Text } from 'react-native';
-import React, { createContext } from 'react';
+import React, { createContext, useContext } from 'react';
 
 const  AuthContext = createContext({});
+
 export const AuthProvider = ({ children }) => {
   return (
-    <AuthContext.Provider value={null}>
+    <AuthContext.Provider 
+        value={{
+            user : 'Crisfiss',
+        }} 
+    >
       { children }
     </AuthContext.Provider>
   );
 };
+
+export default function useAuth() {
+  return useContext(AuthContext);
+}
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import useAuth from '../hooks/useAuth';
 
 const LoginScreen = () => {
+  const { user } = useAuth();
+  console.log(user);
   return (
     <View>
       <Text>Login to the app</Text>
